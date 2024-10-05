142a381d22b87090269c3f2152e84832d2a432e9
Antonio Morales <camoralesc52@gmail.com>
2024-10-04 21:12:22 -0600

    Corregido antes de crear rama independiente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Login } from "./components/auth/Login";
-import { Register } from "./components/auth/Register";
+import { Login } from "./components/auth/login";
+import { Register } from "./components/auth/register";
 
 function App() {
   return <>
