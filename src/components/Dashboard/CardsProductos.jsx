9f62c9744644eb2367f4f9575c1b0b7d389a515f
Juanjo21-01 <chevike15@gmail.com>
2024-10-11 20:17:31 -0600

    Eliminar componentes de productos y modal, reestructurar lista de productos en el dashboard

--- src/components/Dashboard/CardsProductos.jsx
+++ src/components/Dashboard/CardsProductos.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import ProductModal from "../modal/productos";
+import { useState } from 'react';
+import ProductModal from './DatosProductos';
 
 export const ListaProductos = () => {
   const [selectedProduct, setSelectedProduct] = useState(null);
@@ -7,49 +7,49 @@ export const ListaProductos = () => {
   const products = [
     {
       id: 1,
-      nombre: "Manzanita Dorada",
-      precio_unitario: 25.90,
+      nombre: 'Manzanita Dorada',
+      precio_unitario: 25.9,
       stock: 2,
       estado: true,
-      tipo_producto: "Fruta",
+      tipo_producto: 'Fruta',
       proveedor_id: 2,
     },
     {
       id: 2,
-      nombre: "PC Chetada",
+      nombre: 'PC Chetada',
       precio_unitario: 105.99,
       stock: 5,
       estado: true,
-      tipo_producto: "Hogar",
+      tipo_producto: 'Hogar',
       proveedor_id: 3,
     },
     {
       id: 2,
-      nombre: "Dignidad",
+      nombre: 'Dignidad',
       precio_unitario: 9999999.75,
       stock: 0,
       estado: false,
-      tipo_producto: "Vida",
+      tipo_producto: 'Vida',
       proveedor_id: 4,
     },
     {
       id: 2,
-      nombre: "PC Chetada",
+      nombre: 'PC Chetada',
       precio_unitario: 105.99,
       stock: 5,
       estado: true,
-      tipo_producto: "Hogar",
+      tipo_producto: 'Hogar',
       proveedor_id: 3,
     },
     {
       id: 2,
-      nombre: "Dignidad",
+      nombre: 'Dignidad',
       precio_unitario: 9999999.75,
       stock: 0,
       estado: false,
-      tipo_producto: "Vida",
+      tipo_producto: 'Vida',
       proveedor_id: 4,
-    }
+    },
   ];
 
   const handleProductClick = (product) => {
@@ -62,7 +62,9 @@ export const ListaProductos = () => {
 
   return (
     <div className="container mx-auto p-5">
-      <h1 className="text-3xl primary-theme mb-6 text-center font-bold border-b-2 pb-2">Lista de Productos</h1>
+      <h1 className="text-3xl primary-theme mb-6 text-center font-bold border-b-2 pb-2">
+        Lista de Productos
+      </h1>
       <div className="grid xl:grid-cols-4 md:grid-cols-2 gap-5">
         {products.map((product) => (
           <div
@@ -74,9 +76,18 @@ export const ListaProductos = () => {
               <img src="https://picsum.photos/400" alt="IMG Random" />
             </div>
             <div className="pr-4">
-              <h2 className="text-2xl font-bold text-center mb-3 primary-theme border-b pb-1">{product.nombre}</h2>
-              <p className="text-theme">Precio: <span className="secondary-theme">Q{product.precio_unitario}</span></p>
-              <p className="text-theme">Stock: <span className="secondary-theme">{product.stock}</span></p>
+              <h2 className="text-2xl font-bold text-center mb-3 primary-theme border-b pb-1">
+                {product.nombre}
+              </h2>
+              <p className="text-theme">
+                Precio:{' '}
+                <span className="secondary-theme">
+                  Q{product.precio_unitario}
+                </span>
+              </p>
+              <p className="text-theme">
+                Stock: <span className="secondary-theme">{product.stock}</span>
+              </p>
             </div>
           </div>
         ))}
--- src/pages/private/Dashboard.jsx
+++ src/pages/private/Dashboard.jsx
@@ -1,5 +1,12 @@
+import { ListaProductos } from '../../components/Dashboard/CardsProductos';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <ListaProductos />
+    </div>
+  );
 };
 
 export default Dashboard;
